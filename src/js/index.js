99d6d344139b1ef9eab508214eab6b3f0c74a8d5
Jason <timeli@naver.com>
2022-08-08 20:24:39 +0900

    Update: Count 추가 및 빈값 예외처리

--- src/js/index.js
+++ src/js/index.js
@@ -28,6 +28,7 @@
 // step1 요구사항 구현을 위한 전략
 // TODO 메뉴 추가
 // - [ ] 메뉴의 이름을 입력 받고 엔터키 입력으로 추가한다.
+// - [ ] 메뉴의 이름을 입력 받고 확인 버튼을 클릭하면 메뉴를 추가한다.
 // - [ ] 추가되는 메뉴의 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입한다.
 // - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
 // - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
@@ -50,10 +51,16 @@ function App() {
   });
 
   $('#espresso-menu-name').addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-      const espressoMenuName = $('#espresso-menu-name').value;
-      const menuItemTemplate = (espressoMenuName) => {
-        return `
+    if (e.key !== 'Enter') {
+      return;
+    }
+    if ($('#espresso-menu-name').value === '') {
+      alert('값을 입력해 주세요!');
+      return;
+    }
+    const espressoMenuName = $('#espresso-menu-name').value;
+    const menuItemTemplate = (espressoMenuName) => {
+      return `
         <li class="menu-list-item d-flex items-center py-2">
   <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
   <button
@@ -70,13 +77,16 @@ function App() {
   </button>
 </li>
         `;
-      };
+    };
 
-      $('#espresso-menu-list').insertAdjacentHTML(
-        'beforeend',
-        menuItemTemplate(espressoMenuName)
-      );
-    }
+    $('#espresso-menu-list').insertAdjacentHTML(
+      'beforeend',
+      menuItemTemplate(espressoMenuName)
+    );
+
+    const menuCount = $('#espresso-menu-list').querySelectorAll('li').length;
+    $('.menu-count').innerHTML = `총 ${menuCount}개`;
+    $('#espresso-menu-name').value = '';
   });
 }
 
--- src/js/practice.js
+++ src/js/practice.js
@@ -18,30 +18,35 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
-  let espressMenuName;
   $('#espresso-menu-form').addEventListener('submit', (e) => {
     e.preventDefault();
-    let newList = `<li class="menu-list-item d-flex items-center py-2">
-    <span class="w-100 pl-2 menu-name">${espressMenuName}</span>
-    <button
-      type="button"
-      class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-    >
-      수정
-    </button>
-    <button
-      type="button"
-      class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-    >
-      삭제
-    </button>
-  </li>`;
-    $('#espresso-menu-list').insertAdjacentHTML('beforeend', newList);
   });
 
   $('#espresso-menu-name').addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
-      espressMenuName = $('#espresso-menu-name').value;
+      const espressMenuName = $('#espresso-menu-name').value;
+
+      const menuItemTemplate = (espressMenuName) => {
+        return `<li class="menu-list-item d-flex items-center py-2">
+        <span class="w-100 pl-2 menu-name">${espressMenuName}</span>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+        >
+          수정
+        </button>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+        >
+          삭제
+        </button>
+      </li>`;
+      };
+      $('#espresso-menu-list').insertAdjacentHTML(
+        'beforeend',
+        menuItemTemplate(espressMenuName)
+      );
     }
   });
 }
